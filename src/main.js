8d4a00b45c521c7460788eb54a7f4a4fa0c5c838
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-11-12 15:18:37 +0700

    tong 30 so dau tien

--- src/main.js
+++ src/main.js
@@ -2,3 +2,13 @@ var width = 5;
 var height = 10;
 var area = width * height;
 console.log(area);
+var sum = 0;
+var count = 0;
+for (var i = 0; i < 30; i++) {
+    if (i % 2 == 0) {
+        sum += i;
+        count++;
+    }
+}
+console.log(sum);
+console.log(count);
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,14 @@ let width: number = 5;
 let height: number = 10;
 let area: number = width * height;
 console.log(area);
+
+let sum: number = 0;
+let count: number = 0;
+for (let i = 0; i < 30; i++) {
+    if (i % 2 == 0) {
+        sum += i;
+        count++;
+    }
+}
+console.log(sum);
+console.log(count);
